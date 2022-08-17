e855238a3a467d3e4a3815321a69b14fc07104fc
JanoBourian <supermath33@icloud.com>
2022-08-17 18:54:11 -0500

    Add: until validation an test. I need to do more test

--- test/test_crud_engine.py
+++ test/test_crud_engine.py
@@ -22,3 +22,20 @@ async def test_get_all_items_empty():
         response = await ac.get("http://localhost:8000/engine/")
     assert response.status_code == 404
     assert json.loads(response.content) == {"detail": "Nothing item was found"}
+
+@pytest.mark.anyio
+async def test_create_item():
+    data = {
+            "name": "marshall"
+        }
+    async with httpx.AsyncClient() as ac:
+        response = await ac.post("http://localhost:8000/engine/", json = data)
+    # assert response.status_code == 200
+    assert json.loads(response.content) == {"id": 13, "name": "marshall"}
+
+@pytest.mark.anyio
+async def test_delete_item():
+    async with httpx.AsyncClient() as ac:
+        response = await ac.delete("http://localhost:8000/engine/13")
+    # assert response.status_code == 200
+    assert json.loads(response.content) == {"message": "Item 13 was deleted"}
